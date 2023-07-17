e2ee5cb0b2f9a4872aa80bf6fcc89a792a3a26f5
test <adematteo1998@gmail.com>
2023-07-17 13:11:12 -0400

    added variables for characters and letters

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 // Assignment Code
+// Added variables
+
+var specialCharactersList = ['!', "\'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '`', '{', '|', '}', '~'];
+var numberCharacters = ['0', '9', '8', '7', '6', '5', '4', '3', '2', '1'];
+var upperCaseCharacters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowerCaseCharacters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
 var generateBtn = document.querySelector("#generate");
+
+//Add funtion
 
 // Write password to the #password input
 function writePassword() {
